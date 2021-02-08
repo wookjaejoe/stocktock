94e87a05e5b0434c1cd4af4edbb4e37a92ffcd49
positive-man <jwj0104@gmail.com>
2021-02-09 02:11:51 +0900

    Wait until market open and start the simulation

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import datetime
 import logging
 import os
 import sys
+import time
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(os.path.join(basedir, 'stocktock'))
@@ -8,12 +10,21 @@ sys.path.append(os.path.join(basedir, 'stocktock'))
 from utils import log
 
 log.init(logging.DEBUG)
-import time
 
 from simulators import simulators
 
 
 def main():
+    market_open_time = datetime.time(hour=9, minute=0, second=0)
+    while True:
+        now = datetime.datetime.now()
+
+        if now.time() > market_open_time:
+            break
+
+        print('장시작 대기...', end='\r')
+        time.sleep(1)
+
     simulators.Simulator_2().start()
     simulators.Simulator_3().start()
     simulators.Simulator_1().start()
