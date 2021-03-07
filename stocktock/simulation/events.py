4c6724573e61a7e3b6e27f36942e111cd30547ca
positive-man <jwj0104@gmail.com>
2021-03-07 19:55:46 +0900

    Add backend service

--- stocktock/simulation/events.py
+++ stocktock/simulation/events.py
@@ -1,12 +1,10 @@
 import abc
-import logging
 import threading
-from datetime import date, timedelta, datetime
+from datetime import date, timedelta
 from typing import *
 
-import creon.charts
-import creon.stocks
-import database as db
+from creon import charts
+from model import Candle
 
 
 class EventPublisher(abc.ABC):
@@ -25,62 +23,44 @@ class EventPublisher(abc.ABC):
 class MinuteCandleProvdider(EventPublisher):
 
     def __init__(self, code: str, begin: date, end: date):
-        assert len(code) == 6, f'Malformed code: {code}'
-        if not db.Stock.exists(code=code):
-            db.Stock(code=code, name=creon.stocks.get_name(code)).insert(commit=True)
-
         self.code = code
-        self.stock_id = db.Stock.find_by_code(code).id
         self.begin = begin
         self.end = end
-        self.subscribers: List[Callable[[creon.charts.ChartData], None]] = []
+        self.subscribers: List[Callable[[Candle], None]] = []
         self.stopped = False
 
     def _start(self):
-        def run_for(date_at: date):
-            exists = db.MinuteCandle.exists(
-                date=date_at
-            )
+        begin = self.begin
 
-            if not exists:
-                # 해당 일자 캔들이 존재하지 않으면, 크레온에서 받아와 넣는다
-                chart = creon.charts.request_by_term(
-                    code=creon.stocks.find(code=self.code).code,
-                    chart_type=creon.charts.ChartType.MINUTES,
-                    begin=date_at,
-                    end=date_at + timedelta(days=10)
-                )
+        while not self.stopped:
+            end = begin + timedelta(days=10)
+            if end > self.end:
+                end = self.end
+                self.stopped = True
 
-                logging.debug(f'Inserting {len(chart)} candles for {self.code}')
-                db.MinuteCandle.insert_many(
-                    [
-                        db.MinuteCandle(
-                            stock_id=self.stock_id,
-                            date=candle.datetime.date(),
-                            time=candle.datetime.time(),
-                            open=candle.open,
-                            close=candle.close,
-                            low=candle.low,
-                            high=candle.high
-                        ) for candle in chart
-                    ]
-                )
+            chart = charts.request_by_term(code=self.code, chart_type=charts.ChartType.MINUTES, begin=begin, end=end)
+            for candle in chart:
+                if candle.date > self.end:
+                    break
 
-            candles = db.MinuteCandle.query().filter_by(
-                stock_id=self.stock_id,
-                date=date_at
-            ).all()
+                self._publish(candle)
 
-            if candles:
-                candles.sort(key=lambda candle: datetime.combine(candle.date, candle.time))
-                for subscriber in self.subscribers:
-                    for candle in candles:
-                        subscriber(candle)
-
-        cur_date = self.begin
-        while not self.stopped and self.end >= cur_date:
-            run_for(cur_date)
-            cur_date += timedelta(days=1)
+            begin = end + timedelta(days=1)
 
     def stop(self):
         self.stopped = True
+
+    def _publish(self, candle: Candle):
+        for subscriber in self.subscribers:
+            subscriber(candle)
+
+
+class CreonBuiltinEventPublisher(EventPublisher):
+    def _start(self):
+        pass
+
+    def _publish(self, data):
+        pass
+
+# class BreakAbove5MaEventpublisher(EventPublisher):
+#     def __init__(self, price_provider):
