bc8b84b737c237ee00ac2245099739a1e2b0d2de
wookjae_jo <wookjae.jo@ybrain.com>
2021-07-04 18:38:02 +0900

    Prioritize trading based on the Bollinger band middle, lower, and current price gap.

--- stocktock/database/backtest/strategies/bollinger.py
+++ stocktock/database/backtest/strategies/bollinger.py
@@ -6,7 +6,7 @@ from datetime import timedelta, datetime
 from typing import *
 
 from common.model import CandlesGroupByCode
-from database.charts import DayCandlesTable, MinuteCandlesTable
+from database.charts import DayCandlesTable, MinuteCandlesTable, MinuteCandle
 from ..backtest import AbcBacktest
 
 
@@ -107,57 +107,86 @@ class BackTest(AbcBacktest):
 
         blacklist = []
         minute_candles.sort(key=lambda mc: datetime.combine(mc.date, mc.time))
+
+        minute_candles_by_minute = {}
         for minute_candle in minute_candles:
             now = minute_candle.datetime()
-            code = minute_candle.code
-            price = minute_candle.close
+            if now not in minute_candles_by_minute:
+                minute_candles_by_minute.update({now: []})
+            minute_candles_by_minute.get(now).append(minute_candle)
 
-            if self.account.has(code):
-                # 보유중 - 매도 시그널 확인
-                holding = self.account.holdings.get(code)
+        for minute in minute_candles_by_minute:
+            minute_candles = minute_candles_by_minute.get(minute)
 
-                # 최고가 업데이트
-                if price > holding.max:
-                    holding.max = price
+            def sort_by(mc: MinuteCandle):
+                bb = BollingerBand.of(
+                    prices=[dc.close for dc in day_candles_by_code.get(code)[:-1]] + [mc.close],
+                    size=self.BOLLINGER_SIZE
+                )
+                a = bb.mid - bb.lower
+                b = bb.lower - mc.close
+                return a / b
 
-                revenue = price - holding.avg_price
-                revenue_rate = revenue / holding.avg_price * 100
+            minute_candles.sort(key=sort_by, reverse=True)
 
-                if revenue_rate <= self.stop_line:
-                    self._try_sell(when=now, code=code, price=price, amount_rate=1,
-                                   comment=f'손절 {self.stop_line}')
-                    blacklist.append(code)
-                    continue
-
-                if revenue_rate >= self.earning_line_min:
-                    if revenue_rate >= self.earning_line_max:
-                        self._try_sell(when=now, code=code, price=price, amount_rate=1,
-                                       comment=f'전량 익절 {self.earning_line_max}%+')
-                        continue
-                    if (price - holding.max) / holding.max * 100 < -abs(self.trailing_stop_rate):  # 트레일링 스탑
-                        self._try_sell(when=now, code=code, price=price, amount_rate=1,
-                                       comment=f'트레일링 스탑: 고점({holding.max}) 대비 {(price - holding.max) / holding.max * 100}%')
-                        continue
-                    if revenue_rate >= self.earning_line and not hasattr(holding, 'mark'):
-                        holding.mark = True
-                        self._try_sell(when=now, code=code, price=price, amount_rate=1 / 2,
-                                       comment=f'절반 익절 {self.earning_line}%+')
-                        continue
-
-            else:
-                if code in blacklist:
-                    continue
-
+            for minute_candle in minute_candles:
+                now = minute_candle.datetime()
+                code = minute_candle.code
+                price = minute_candle.close
                 bollinger = BollingerBand.of(
                     prices=[dc.close for dc in day_candles_by_code.get(code)[:-1]] + [price],
                     size=self.BOLLINGER_SIZE
                 )
 
-                # 미보유 - 매수 시그널 확인
-                if minute_candle.close < bollinger.lower:
-                    self._try_buy(
-                        when=now, code=code,
-                        price=minute_candle.close,
-                        amount=self.once_buy_amount,
-                        comment='현재가 밴드 하단'
-                    )
+                if self.account.has(code):
+                    # 보유중 - 매도 시그널 확인
+                    holding = self.account.holdings.get(code)
+
+                    # 최고가 업데이트
+                    if price > holding.max:
+                        holding.max = price
+
+                    revenue = price - holding.avg_price
+                    revenue_rate = revenue / holding.avg_price * 100
+
+                    if price > bollinger.mid:
+                        self._try_sell(when=now, code=code, price=price, amount_rate=1,
+                                       comment=f'현재가 밴드 중단')
+                        if revenue_rate < 0:
+                            blacklist.append(code)
+
+                        continue
+
+                    # if revenue_rate <= self.stop_line:
+                    #     self._try_sell(when=now, code=code, price=price, amount_rate=1,
+                    #                    comment=f'손절 {self.stop_line}')
+                    #     blacklist.append(code)
+                    #     continue
+                    #
+                    # if revenue_rate >= self.earning_line_min:
+                    #     if revenue_rate >= self.earning_line_max:
+                    #         self._try_sell(when=now, code=code, price=price, amount_rate=1,
+                    #                        comment=f'전량 익절 {self.earning_line_max}%+')
+                    #         continue
+                    #     if (price - holding.max) / holding.max * 100 < -abs(self.trailing_stop_rate):  # 트레일링 스탑
+                    #         self._try_sell(when=now, code=code, price=price, amount_rate=1,
+                    #                        comment=f'트레일링 스탑: 고점({holding.max}) 대비 {(price - holding.max) / holding.max * 100}%')
+                    #         continue
+                    #     if revenue_rate >= self.earning_line and not hasattr(holding, 'mark'):
+                    #         holding.mark = True
+                    #         self._try_sell(when=now, code=code, price=price, amount_rate=1 / 2,
+                    #                        comment=f'절반 익절 {self.earning_line}%+')
+                    #         continue
+
+                else:
+                    if code in blacklist:
+                        continue
+
+                    # 미보유 - 매수 시그널 확인
+                    if price < bollinger.lower:
+                        self._try_buy(
+                            when=now, code=code,
+                            price=minute_candle.close,
+                            amount=self.once_buy_amount,
+                            comment='현재가 밴드 하단'
+                        )
