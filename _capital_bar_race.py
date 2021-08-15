b5615b405484dd3a3c848284aad2b9a228a79080
positive-man <jwj0104@gmail.com>
2021-08-16 01:35:21 +0900

    Fixed an issue where the item name was displayed incorrectly

--- _capital_bar_race.py
+++ _capital_bar_race.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
 
-import calendar
+from dataclasses import dataclass
 from datetime import date, timedelta
 
 import pandas
+from pykrx import stock
 
-import krx
 from database import fundamental
-from dataclasses import dataclass
 
 
 @dataclass
@@ -54,9 +53,22 @@ def group_by(l: list, grouping_key, sorting_key):
     return result
 
 
+def get_names_by_code(target_date: date):
+    d = target_date
+    while True:
+        assert abs(target_date - d) < timedelta(days=30), 'Something wrong...'
+        result = stock.krx.get_market_ticker_and_name(date=d.strftime('%Y%m%d'), market='ALL').to_dict()
+        if not result:
+            print('???')
+            d += timedelta(days=1)
+            continue
+
+        return result
+
+
 def main():
     from_ym = YearMonth(1996, 1)
-    to_ym = YearMonth.now()
+    to_ym = YearMonth(2021, 7)
     ym = from_ym
 
     df = pandas.DataFrame()
@@ -71,14 +83,26 @@ def main():
             capitals = [capitals_by_code.get(code)[-1] for code in capitals_by_code]
             capitals.sort(key=lambda x: x.cap, reverse=True)
             capitals = capitals[:30]
-            assert capitals
+            assert capitals, 'Something wrong...'
+
+            names_by_code = get_names_by_code(capitals[0].date)
+            blacklist = []
+            for c in capitals:
+                if c.code not in names_by_code:
+                    print(f'[WARN] Cannot found name for {c.code}')
+                    blacklist.append(c)
+
+            for c in blacklist:
+                capitals.remove(c)
+
             df = pandas.merge(
-                df, pandas.DataFrame({str(ym): {krx.get_name(c.code): c.cap for c in capitals}}),
+                df, pandas.DataFrame({str(ym): {names_by_code.get(c.code): int(c.cap / 10000_0000) for c in capitals}}),
                 how="outer", left_index=True, right_index=True
             )
 
             ym = ym.next()
 
+    df.fillna(0, inplace=True)
     df.to_csv('bar_race.csv')
 
 
